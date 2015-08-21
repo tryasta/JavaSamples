01b7e84473fc0b61dfab5f8b04c0fde3deea243b
sayembd <sayem64@gmail.com>
2015-08-21 15:07:46 +0900

    Algorithms
    Singly linked list implementation - added test case for count and insert operation.

---
+++ algorithms/src/main/java/com/keertimaan/algorithms/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2015 Sayem Ahmed
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.keertimaan.algorithms.linkedlist;
+
+/**
+ * @author <a href="http://www.codesod.com">Sayem Ahmed</a>
+ */
+public class SinglyLinkedList<T> {
+  private Node<T> head;
+  private int count;
+
+  public int count() {
+    return count;
+  }
+
+  public void insert(T value) {
+    if (head == null) {
+      head = new Node<>(value);
+    } else {
+      head.next = new Node<>(value);
+    }
+    count++;
+  }
+
+  private static class Node<T> {
+    private T value;
+    private Node<T> next;
+
+    public Node(T value) {
+      this.value = value;
+    }
+  }
+}
