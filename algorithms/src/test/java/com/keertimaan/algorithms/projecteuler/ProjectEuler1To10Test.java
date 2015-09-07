b6bd953929a47eb40a4cc386f21123bb34fb904a
sayembd <sayem64@gmail.com>
2015-09-07 10:55:18 +0900

    Algorithms
    https://projecteuler.net/problem=2

--- algorithms/src/test/java/com/keertimaan/algorithms/projecteuler/ProjectEuler1To10Test.java
+++ algorithms/src/test/java/com/keertimaan/algorithms/projecteuler/ProjectEuler1To10Test.java
@@ -28,4 +28,16 @@ public class ProjectEuler1To10Test {
   public void testMultipleOf3And5LessThan() {
     assertThat(ProjectEuler1To10.multipleOf3And5LessThan(1000)).isEqualTo(233168);
   }
+
+  @Test
+  public void testFibonacciEvenSum() {
+    assertThat(ProjectEuler1To10.fibonacciEvenSum(-1)).isEqualTo(0);
+    assertThat(ProjectEuler1To10.fibonacciEvenSum(0)).isEqualTo(0);
+    assertThat(ProjectEuler1To10.fibonacciEvenSum(1)).isEqualTo(0);
+    assertThat(ProjectEuler1To10.fibonacciEvenSum(2)).isEqualTo(2);
+    assertThat(ProjectEuler1To10.fibonacciEvenSum(3)).isEqualTo(2);
+    assertThat(ProjectEuler1To10.fibonacciEvenSum(8)).isEqualTo(10);
+    assertThat(ProjectEuler1To10.fibonacciEvenSum(34)).isEqualTo(44);
+    assertThat(ProjectEuler1To10.fibonacciEvenSum(4000000)).isEqualTo(4613732);
+  }
 }
